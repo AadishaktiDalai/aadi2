4b8edeec36f92b0cb146d7534c0edb888dba969f
AadishaktiDalai <42179020+AadishaktiDalai@users.noreply.github.com>
2018-08-14 16:22:34 +0530

    Add files via upload

---
+++ aadilab2_1.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "What is your name ?"
+read AADI
+mkdir $AADI
+cd $AADI
+touch $AADI.cpp
---
+++ aadilab2_2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "What is your name ?"
+read AADI
+mkdir $AADI
+cd $AADI
+touch aadish1.cpp
+touch aadish2.cpp
+touch aadish3.cpp
+touch aadish4.cpp
+touch aadish5.cpp
